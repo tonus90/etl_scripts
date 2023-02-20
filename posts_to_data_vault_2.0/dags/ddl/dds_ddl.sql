92b0dd929fc5f0b83e78026af0aa85a26e959780
tonus90 <74110011+tonus90@users.noreply.github.com>
2023-02-20 18:36:04 +0300

    Update dds_ddl.sql

--- posts_to_data_vault_2.0/dags/ddl/dds_ddl.sql
+++ posts_to_data_vault_2.0/dags/ddl/dds_ddl.sql
@@ -21,12 +21,11 @@ CREATE TABLE dds.hub_post (
 -- Create table link users_posts
 DROP TABLE IF EXISTS dds.link_user_post CASCADE;
 CREATE TABLE IF NOT EXISTS dds.link_user_post (
-  user_id INT NOT NULL,
-  post_id INT NOT NULL,
-  user_post_hash_key CHAR(32) NOT NULL,
+  post_hash_key CHAR(32) NOT NULL,
+  user_hash_key CHAR(32) NOT NULL,
   load_date TIMESTAMP,
   source_system VARCHAR(50),
-  PRIMARY KEY (user_id, post_id),
+  PRIMARY KEY (post_hash_key, user_hash_key),
   unique (user_post_hash_key)
 );
 
